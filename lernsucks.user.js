c7bc531eb9dc95256141b7d86a70f13630eed61a
Tobias Möritz <tobias@moeritz.cc>
2020-11-05 16:38:39 +0100

    Update lernsucks.user.js

--- lernsucks.user.js
+++ lernsucks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LernSucks
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      2.0
 // @description  Make LernSax.de appreciateable
 // @author       tobimori
 // @match        *://*.lernsax.de/*
@@ -10,16 +10,22 @@
 
 function walkText(node) {
   if (node.nodeType == 3) {
-    node.data = node.data.replace(/ax/g, "ucks");
-    node.data = node.data.replace(/achsen/g, "ucksen");
-    node.data = node.data.replace(/ächsisch/g, "ucksisch");
-    node.data = node.data.replace(/Deutsch/g, "Toitsch");
+    node.data = node.data
+        .replace(/([sS])ax/g, "$1ucks")
+        .replace(/([sS])achsen/g, "$1ucksen")
+        .replace(/([sS])ächsisch/g, "$1ucksisch")
+        .replace(/([dD])eutsch/g, "$1oitsch")
+        .replace(/Schulsport/g, "Folter")
+        .replace(/Landesamt für Schule und Bildung/g, "Landesamt für Corona-Superspreading und Unverantwortlichkeit")
+        .replace(/Schuldatenbank/g, "Datenbank für Corona-Superspreader")
+        .replace(/Schule/g, "Corona-Superspreading")
+        .replace(/Schul(.)/g, x => "Corona-Superspreading-" + x[5].toUpperCase())
   }
   if (node.nodeType == 1 && node.nodeName != "SCRIPT") {
     for (var i = 0; i < node.childNodes.length; i++) {
-      walkText(node.childNodes[i]);
+      walkText(node.childNodes[i])
     }
   }
 }
-walkText(document.body);
-walkText(document.head);
+walkText(document.body)
+walkText(document.head)
